edd2269f5ac5573b3ef403b6f4b880e025c282ae
hgim96715-lgtm <hgim96715@gmail.com>
2026-03-22 11:18:18 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 9.01 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,9 @@
+def solution(id_pw, db):
+    for id,pw in db:
+        if id_pw[0]==id:
+            if id_pw[1]==pw:
+                return 'login'
+            else:
+                return 'wrong pw'
+    else:
+        return 'fail'
