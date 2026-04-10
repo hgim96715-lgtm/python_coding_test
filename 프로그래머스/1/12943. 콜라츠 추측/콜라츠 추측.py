b1aed26b17f81384dfab02887f478c9d3294de86
hgim96715-lgtm <hgim96715@gmail.com>
2026-04-11 06:24:17 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.10 ms, Memory: 9.05 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,10 +1,9 @@
 def solution(num):
     answer = 0
-    while num>1:
+    while num!=1:
         if num%2:
             num=num*3+1
-            answer+=1
         else:
-            num/=2
-            answer+=1
+            num//=2
+        answer+=1
     return answer if answer<=500 else -1
